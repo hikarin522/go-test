16a05a3a296f7bb61511288cd74bcda5d02fb80e
＼ﾋｯｶﾘｰﾝ／ <s.nlf.fh@gmail.com>
2014-12-11 10:16:12 +0900

    update

--- revelApp/revelwiki/app/controllers/app.go
+++ revelApp/revelwiki/app/controllers/app.go
@@ -3,8 +3,10 @@ package controllers
 import (
 	"fmt"
 	"github.com/revel/revel"
-	"io/ioutil"
+	//"io/ioutil"
+	"regexp"
 	"revelApp/revelwiki/app/routes"
+	//"runtime"
 )
 
 type App struct {
@@ -15,24 +17,40 @@ func (c App) Index() revel.Result {
 	return c.Render()
 }
 
-func loadPage(title string) string {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		fmt.Println("title:", title)
-		fmt.Println("Load Error:", err)
-		return ""
-	}
+var page = make(map[string]string)
 
-	return string(body)
+func loadPage(title string) string {
+	return page[title]
+	/*
+		filename := title + ".txt"
+		body, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Println("title:", title)
+			fmt.Println("Load Error:", err)
+			return ""
+		}
+
+		return string(body)
+	*/
 }
 
 func savePage(title string, body string) error {
-	filename := title + ".txt"
-	return ioutil.WriteFile(filename, []byte(body), 0600)
+	page[title] = body
+	return nil
+	/*
+		filename := title + ".txt"
+		return ioutil.WriteFile(filename, []byte(body), 0600)
+	*/
 }
 
+var titleValid = regexp.MustCompile("^([a-zA-Z0-9]+)$")
+
 func (c App) View(title string) revel.Result {
+	c.Validation.Match(title, titleValid)
+	if c.Validation.HasErrors() {
+		return c.NotFound(title)
+	}
+
 	body := loadPage(title)
 	if body == "" {
 		return c.Redirect(routes.App.Edit(title))
@@ -41,11 +59,21 @@ func (c App) View(title string) revel.Result {
 }
 
 func (c App) Edit(title string) revel.Result {
+	c.Validation.Match(title, titleValid)
+	if c.Validation.HasErrors() {
+		return c.NotFound(title)
+	}
+
 	body := loadPage(title)
 	return c.Render(title, body)
 }
 
 func (c App) Save(title, body string) revel.Result {
+	c.Validation.Match(title, titleValid)
+	if c.Validation.HasErrors() {
+		return c.NotFound(title)
+	}
+
 	err := savePage(title, body)
 	if err != nil {
 		fmt.Println("title:", title)
